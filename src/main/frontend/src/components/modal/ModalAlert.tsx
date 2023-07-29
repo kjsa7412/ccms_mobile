45c7c5e19b6c48b1ac1b496ad8aea2ea3a948cef
kjsa <kjsa7412@gmail.com>
2023-07-29 15:59:35 +0900

    Clean Package

--- src/main/frontend/src/components/modal/ModalAlert.tsx
+++ src/main/frontend/src/components/modal/ModalAlert.tsx
@@ -1,4 +1,3 @@
-import ReactModal from 'react-modal';
 import './ModalAlert.css';
 
 type Props = {
@@ -13,40 +12,22 @@ const modalAlert = ({modalIsOpen, funcCloseModal, message}: Props) => {
     };
 
     return (
-        <ReactModal isOpen={modalIsOpen}
-                    ariaHideApp={false}
-                    style={{
-                        overlay: {
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            background: 'rgba(0, 0, 0, 0.2)',
-                            zIndex: 3,
-                        },
-                        content: {
-                            position: 'static',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            background: 'white',
-                            overflow: 'auto',
-                            borderRadius: '10px',
-                            outline: 'none',
-                            borderStyle: 'none',
-                            borderColor: 'darkgray',
-                            width: '250px',
-                            height: '200px'
-                        }
-                    }}>
-            <div className={"modalAlert-textContainer"}>
-                <p>{message}</p>
-            </div>
-            <div className={"modalAlert-ok"} onClick={CloseModal}>
-                확인
-            </div>
-        </ReactModal>
+        <>
+            {modalIsOpen &&
+                <div className={"modalAlert-overlay"}>
+                    <div className={"modalAlert-modal"}>
+                        <div className={"modalAlert-textContainer"}>
+                            <p>{message}</p>
+                        </div>
+                        <div className={"modalAlert-ok"} onClick={CloseModal}>
+                            확인
+                        </div>
+                    </div>
+                </div>
+            }
+        </>
     )
 }
+
 
 export default modalAlert;
--- src/main/frontend/src/components/modal/ModalMutation.tsx
+++ src/main/frontend/src/components/modal/ModalMutation.tsx
@@ -1,4 +1,3 @@
-import ReactModal from 'react-modal';
 import './ModalMutation.css';
 import {EModalMutationStatus} from "@custom-enums/common-enum";
 
@@ -16,101 +15,82 @@ const modalMutation = ({
                            successFunction
                        }: Props) => {
     return (
-        <ReactModal isOpen={modalMutationStatus !== EModalMutationStatus.Close}
-                    ariaHideApp={false}
-                    style={{
-                        overlay: {
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            background: 'rgba(0, 0, 0, 0.2)',
-                            zIndex: 3,
-                        },
-                        content: {
-                            position: 'static',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            background: 'white',
-                            overflow: 'auto',
-                            borderRadius: '10px',
-                            outline: 'none',
-                            borderStyle: 'none',
-                            borderColor: 'darkgray',
-                            width: '250px',
-                            height: '200px'
-                        }
-                    }}>
+        <>
+            {
+                modalMutationStatus !== EModalMutationStatus.Close &&
+                <div className={"modalAlert-overlay"}>
+                    <div className={"modalAlert-modal"}>
+                        {modalMutationStatus === EModalMutationStatus.Confirm && (
+                            <>
+                                <div className={"modalMutation-textContainer"}>
+                                    <p>진행하시겠습니까?</p>
+                                </div>
+                                <div className={"modalMutation-button"}
+                                     onClick={() => {
+                                         setModalMutationStatus(EModalMutationStatus.Progress);
+                                         resultMutation.mutate();
+                                     }}
+                                >
+                                    확인
+                                </div>
+                                <div className={"modalMutation-button"}
+                                     onClick={() => {
+                                         setModalMutationStatus(EModalMutationStatus.Close);
+                                     }}
+                                >
+                                    취소
+                                </div>
+                            </>
+                        )}
 
-            {modalMutationStatus === EModalMutationStatus.Confirm && (
-                <>
-                    <div className={"modalMutation-textContainer"}>
-                        <p>진행하시겠습니까?</p>
-                    </div>
-                    <div className={"modalMutation-button"}
-                         onClick={() => {
-                             setModalMutationStatus(EModalMutationStatus.Progress);
-                             resultMutation.mutate();
-                         }}
-                    >
-                        확인
-                    </div>
-                    <div className={"modalMutation-button"}
-                         onClick={() => {
-                             setModalMutationStatus(EModalMutationStatus.Close);
-                         }}
-                    >
-                        취소
-                    </div>
-                </>
-            )}
+                        {modalMutationStatus === EModalMutationStatus.Progress && (
+                            <>
+                                <div className={"modalMutation-textContainer"}>
+                                    <p>처리중입니다.</p>
+                                </div>
+                                <div className={"modalMutation-button"}>
+                                    <img alt={'spinner'} src={`${process.env.PUBLIC_URL}/img/Spinner-1s-38px.gif`}/>
+                                </div>
+                            </>
+                        )}
 
-            {modalMutationStatus === EModalMutationStatus.Progress && (
-                <>
-                    <div className={"modalMutation-textContainer"}>
-                        <p>처리중입니다.</p>
-                    </div>
-                    <div className={"modalMutation-button"}>
-                        <img alt={'spinner'} src={`${process.env.PUBLIC_URL}/img/Spinner-1s-38px.gif`}/>
-                    </div>
-                </>
-            )}
+                        {modalMutationStatus === EModalMutationStatus.Success && (
+                            <>
+                                <div className={"modalMutation-textContainer"}>
+                                    <p>처리되었습니다.</p>
+                                </div>
+                                <div className={"modalMutation-button"}
+                                     onClick={() => {
+                                         resultMutation.reset();
+                                         setModalMutationStatus(EModalMutationStatus.Close);
+                                         successFunction();
+                                     }}
+                                >
+                                    확인
+                                </div>
+                            </>
+                        )}
 
-            {modalMutationStatus === EModalMutationStatus.Success && (
-                <>
-                    <div className={"modalMutation-textContainer"}>
-                        <p>처리되었습니다.</p>
+                        {modalMutationStatus === EModalMutationStatus.Error && (
+                            <>
+                                <div className={"modalMutation-textContainer"}>
+                                    <p>실패했습니다.</p>
+                                </div>
+                                <div className={"modalMutation-button"}
+                                     color={'red'}
+                                     onClick={() => {
+                                         resultMutation.reset();
+                                         setModalMutationStatus(EModalMutationStatus.Close);
+                                     }}
+                                >
+                                    확인
+                                </div>
+                            </>
+                        )}
                     </div>
-                    <div className={"modalMutation-button"}
-                         onClick={() => {
-                             resultMutation.reset();
-                             setModalMutationStatus(EModalMutationStatus.Close);
-                             successFunction();
-                         }}
-                    >
-                        확인
-                    </div>
-                </>
-            )}
-
-            {modalMutationStatus === EModalMutationStatus.Error && (
-                <>
-                    <div className={"modalMutation-textContainer"}>
-                        <p>실패했습니다.</p>
-                    </div>
-                    <div className={"modalMutation-button"}
-                         color={'red'}
-                         onClick={() => {
-                             resultMutation.reset();
-                             setModalMutationStatus(EModalMutationStatus.Close);
-                         }}
-                    >
-                        확인
-                    </div>
-                </>
-            )}
-        </ReactModal>
+                </div>
+            }
+        </>
     )
 }
 
