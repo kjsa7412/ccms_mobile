3030ddaad525c90422a07723e03c0b7a99e4e68c
kjsa <kjsa7412@gmail.com>
2023-09-01 16:28:29 +0900

    Change Font

--- src/main/frontend/src/components/GlobalHelmet.tsx
+++ src/main/frontend/src/components/GlobalHelmet.tsx
@@ -6,9 +6,7 @@ const GlobalHelmet = () => {
         <Helmet>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
-            <link
-                href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;400;700&family=Noto+Sans:wght@100;400;700&display=swap"
-                rel="stylesheet"/>
+            <link href="https://cdn.jsdelivr.net/gh/sunn-us/SUITE/fonts/static/woff2/SUITE.css" rel="stylesheet"/>
         </Helmet>
     )
 }
